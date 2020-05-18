e789f1dcf0d64d515d1eb03dafdad22abc70fc0d
Stefan Ruedisuehli <stefan.ruedisuehli@env.ethz.ch>
2020-05-18 19:08:27 +0200

    extend wsl memory free command

--- free_wsl2_memory.sh
+++ free_wsl2_memory.sh
@@ -3,4 +3,4 @@
 # Free unused memory claimed by linux in WSL2
 # src: https://devblogs.microsoft.com/commandline/memory-reclaim-in-the-windows-subsystem-for-linux-2/
 echo "root password required"
-su -c 'echo 1 > /proc/sys/vm/drop_caches'
+su -c 'echo 1 > /proc/sys/vm/compact_memory && echo 1 > /proc/sys/vm/drop_caches'
