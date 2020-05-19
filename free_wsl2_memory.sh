d0e0af5ae86bd8dc8b3301487a5fd35c063be4e3
Stefan Ruedisuehli <stefan.ruedisuehli@env.ethz.ch>
2020-05-19 07:12:12 +0200

    skip su if already root

--- free_wsl2_memory.sh
+++ free_wsl2_memory.sh
@@ -2,5 +2,21 @@
 
 # Free unused memory claimed by linux in WSL2
 # src: https://devblogs.microsoft.com/commandline/memory-reclaim-in-the-windows-subsystem-for-linux-2/
-echo "root password required"
-su -c 'echo 1 > /proc/sys/vm/compact_memory && echo 1 > /proc/sys/vm/drop_caches'
+free_memory()
+{
+    echo "echo 1 > /proc/sys/vm/compact_memory"
+    echo 1 > /proc/sys/vm/compact_memory
+    echo "echo 1 > /proc/sys/vm/drop_caches"
+    echo 1 > /proc/sys/vm/drop_caches
+}
+
+case "$(whoami)" in
+    root)
+        free_memory
+    ;;
+    *)
+        echo "root password required"
+        export -f free_memory
+        su -c free_memory
+    ;;
+esac
